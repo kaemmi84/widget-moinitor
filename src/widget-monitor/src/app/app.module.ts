50a86fb282acb9a32e3a2dea92c6d72f2acf157b
Steffen Kämmerer <steffen.kaemmerer@zeiss.com>
2021-10-02 11:34:35 +0200

    added widget components

--- src/widget-monitor/src/app/app.module.ts
+++ src/widget-monitor/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TramComponent } from './widgets/tram/tram.component';
+import { ClockComponent } from './widgets/clock/clock.component';
+import { StockComponent } from './widgets/stock/stock.component';
+import { WeatherComponent } from './widgets/weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TramComponent,
+    ClockComponent,
+    StockComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/widget-monitor/src/app/widgets/tram/tram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tram',
+  templateUrl: './tram.component.html',
+  styleUrls: ['./tram.component.scss']
+})
+export class TramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
